e8294c1a38c8e220618363362adfafa48a2f600e
Sam <45475939+SamJessep@users.noreply.github.com>
2020-02-05 00:48:07 +1300

    Update Main.js

--- public/js/Main.js
+++ public/js/Main.js
@@ -13,7 +13,7 @@ $('#search').on('input', function(){
 window.addEventListener('scroll', throttle(infinateLoad, 500));
 window.onscroll = function(ev) {
   app.showScrollButton()
-  if(app.isOnBottom() && app.getEl('detailed').classList.contains('hidden')){
+  if(app.isOnBottom() && app.getEl('details').classList.contains('hidden')){
     app.loadMore();
   }
 }
