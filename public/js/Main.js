58918802fe03e5de529d44cff37990020bda4aca
SamJessep <samjessep77@gmail.com>
2020-02-11 19:52:16 +1300

    reverted to before login added

--- public/js/Main.js
+++ public/js/Main.js
@@ -1,21 +1,10 @@
 //console.log(Preferences.fetchData());
 let savedPreferences = Preferences.fetchData();
 let preferencesObject = new Preferences()
-let REQUEST_TOKEN = getParameterByName('request_token');
 if(savedPreferences){
   Preferences.LoadData(preferencesObject,savedPreferences)
 }
 const app = new App(preferencesObject);
-let btn = $('#loginButton')[0];
-if(getParameterByName('approved') == 'true' && REQUEST_TOKEN){
-  console.log('logged in')
-  preferencesObject.Session_token = REQUEST_TOKEN;
-  //preferencesObject.savePreferences();
-  btn.innerText = "Logout"
-  btn.onclick = logout;
-}else{
-  //btn.innerText = 'Login';
-}
 
 $('#search').on('input', function(){
   app.getAutoCompleteItems(this);
@@ -32,36 +21,4 @@ function infinateLoad(){
   if(app.scrollIsNearBottom() && app.getEl('details').classList.contains('hidden')){
     app.loadMore()
   }
-}
-
-function getKey(){
-  let URL = `https://api.themoviedb.org/3/authentication/token/new`
-  let filters = {
-    api_key: '579872d8976e8f07d27624584808fee2'
-  }
-  SendReq(URL, {'success':login}, filters)
-}
-
-function login(data){
-  let token = data.request_token;
-  let URL = `https://www.themoviedb.org/authenticate/${token}?redirect_to=http://moviedb-77.netlify.com/`
-  window.open(URL);
-}
-
-function logout(){
-  let URL = 'https://api.themoviedb.org/3/authentication/session?api_key=579872d8976e8f07d27624584808fee2'
-  $.ajax({
-    url: URL,
-    type: 'DELETE',
-    data: {
-      session_id: REQUEST_TOKEN
-    },
-    success: function(response){
-      let page = window.location.href.split('?')[0]+window.location.hash;
-      window.location = page;
-    },
-    error: function(response){
-      console.error(response)
-    },
-  })
 }
