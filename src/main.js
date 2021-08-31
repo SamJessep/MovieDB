c6252fa0947a78077ccb73f611b84cc235771ed0
sam <samjessep77@gmail.com>
2021-08-31 20:26:37 +1200

    re enabled service worker

--- src/main.js
+++ src/main.js
@@ -6,6 +6,6 @@ const app = new App({
 });
 
 if ('serviceWorker' in navigator) {
-  // navigator.serviceWorker.register('/sw.js')
+  navigator.serviceWorker.register('/sw.js')
 }
 export default app;
